2193dc75b6447b31f586fb31467eaa1cb2896551
wgs11 <4teatoo@gmail.com>
2018-03-14 03:24:32 -0400

    Categories

---
+++ db/migrate/20180314072041_create_categories.rb
@@ -0,0 +1,12 @@
+class CreateCategories < ActiveRecord::Migration[5.1]
+  def change
+    create_table :categories do |t|
+      t.string :game_id
+      t.string :cat_name
+      t.integer :time
+      t.string :video_url
+
+      t.timestamps
+    end
+  end
+end
