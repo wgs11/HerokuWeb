b5a7cbd3ff7953c1bc8bafcbbb61b3363f5eaece
wgs11 <4teatoo@gmail.com>
2018-03-18 01:29:27 -0400

    more web stuff

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,6 +1,6 @@
 class DonationsController < ApplicationController
   before_action :set_donation, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :verify_authenticity_token
   # GET /donations
   # GET /donations.json
   def index
